c201c64913f72d22fc6fb0a42c3c24f6dffaeff7
shehrozAhmar <shehrozahmar17@gmail.com>
2023-12-23 21:09:49 +0200

    main commit

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,2 @@
-# Changes from the current branch (main)
-print("Hello, World! from main")
-# Changes from the other branch (master3/master)
-print("Hello, World! from master3/master")
+# hello_world.py
+print("Hello World!")
